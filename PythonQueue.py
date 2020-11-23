a89d12215cbde39419c9d0496022435601ab7f94
EstuarC <71733988+EstuarC@users.noreply.github.com>
2020-11-23 16:21:49 -0500

    Add files via upload

---
+++ PythonQueue.py
@@ -0,0 +1,25 @@
+names = []
+
+for x in range(0,10):
+  Name = input("Enter a Name: ")
+  names.append(Name)
+  print(names)
+
+for x in range(0,10):
+  print(names.pop(0))
+  print(names)
+
+
+
+
+    
+  
+    
+          
+
+
+
+   
+   
+
+   
